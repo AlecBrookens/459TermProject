942939481881a3eeade689d11eabafbe811bb1d1
Alec Brookens <123490227+AlecBrookens@users.noreply.github.com>
2024-12-09 15:33:04 -0600

    all set

--- 3DLighting.c
+++ 3DLighting.c
@@ -270,7 +270,7 @@ int main(int argc, char **argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(800, 600);
-    glutCreateWindow("Static Mesh with Multiple Lights");
+    glutCreateWindow("3D Lighting Term Project Demo");
 
     if (!init()) return -1;
 
--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-all: Chad.c
-	gcc -Wall -Wextra -std=c99 -o out Chad.c -lGL -lGLU -lglut -lm
+all: 3DLightning.c
+	gcc -Wall -Wextra -std=c99 -o out 3DLighting.c -lGL -lGLU -lglut -lm
 
 clean:
 	rm -f out
